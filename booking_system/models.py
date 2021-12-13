370cbc7aae929865b1d2e8fdf8062da1422ef366
jenns93 <jjenns93@gmail.com>
2021-12-13 03:33:52 +0000

    Add authentication, sign up/ log in/ log out

--- booking_system/models.py
+++ booking_system/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
 STATUS = ((0, "Draft"), (1, "Published"))
  
